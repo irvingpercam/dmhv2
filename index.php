db6eccd3aecccd4917c43c53d1f1db73a9164f24
Irving Alain Aguilar Pérez <a01703171@itesm.mx>
2019-09-15 16:07:47 -0500

    Upload: files upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include("sections/_header.php") ?>
+<?php include("sections/_navbar.php") ?>
+<?php include("sections/inicio/_inicio.php") ?>
+<?php include("sections/_footer.php") ?>
---
+++ sections/_footer.php
@@ -0,0 +1,15 @@
+<div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
