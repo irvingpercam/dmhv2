e18ff7d9acbafcfa2f3d219e816c46fb7ea01864
Irving Alain Aguilar Pérez <a01703171@itesm.mx>
2020-01-09 17:29:58 -0600

    Update: January 2020

---
+++ views/contacto.php
@@ -0,0 +1,4 @@
+<?php include("../sections/_header.php") ?>
+<?php include("../sections/_navbar.php") ?>
+<?php include("../sections/contacto/_contacto.php") ?>
+<?php include("../sections/_footer.php") ?>
